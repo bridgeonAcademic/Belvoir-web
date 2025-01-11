8cc4008031fc4251ade411a0187f09a41c2f88d7
arshida <arshidaakbar39@gmail.com>
2025-01-11 17:15:56 +0530

    admin updated

--- src/api/rentalProducts-api.js
+++ src/api/rentalProducts-api.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const fetchRentalProducts = async ({ queryKey }) => {
-  const [,pagenumber, pagesize] = queryKey;
-  console.log(pagenumber,pagesize)
-  const response = await axios.get(
-    `https://belvoir-rest-api.onrender.com/api/Rentals/paginated`,
-    { params: { pagenumber, pagesize } }
-  );
+// export const fetchRentalProducts = async ({ queryKey }) => {
+//   const [,pagenumber, pagesize] = queryKey;
+//   console.log(pagenumber,pagesize)
+//   const response = await axios.get(
+//     `https://belvoir-rest-api.onrender.com/api/Rentals/paginated`,
+//     { params: { pagenumber, pagesize } }
+//   );
 
-  if (!response.ok) throw new Error("Failed to fetch users");
-  return response.data;
-};
+//   if (!response.ok) throw new Error("Failed to fetch users");
+//   return response.data;
+// };
 
-export const editRentalProducts = async () => {
-  const response = await axios.put(
-    "https://belvoir-rest-api.onrender.com/api/Rentals/update?id=${id}"
-  );
-  return response.data;
-};
+// export const editRentalProducts = async () => {
+//   const response = await axios.put(
+//     "https://belvoir-rest-api.onrender.com/api/Rentals/update?id=${id}"
+//   );
+//   return response.data;
+// };
